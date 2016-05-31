145be08cace8e30a0498b1029819c4c94569f425
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-05-31 16:11:07 +0200

    modified:   Callers/DELLY.sh 	modified:   Callers/TIDDIT_simulated.sh 	modified:   Callers/fermikit.slurm 	new file:   Simulated_data/filtered_Lumpy_TIDDIT_manta/benchmark.filtered_Manta.vcf 	new file:   Simulated_data/filtered_Lumpy_TIDDIT_manta/benchmark.filtered_TIDDIT.vcf 	new file:   Simulated_data/filtered_Lumpy_TIDDIT_manta/benchmark.filtered_lumpy.vcf 	new file:   Simulated_data/filtered_Lumpy_TIDDIT_manta/filter_vcf.py 	new file:   Simulated_data/filtered_Lumpy_TIDDIT_manta/filter_vcf.py~ 	new file:   Simulated_data/slurm/slurm-7898835.out 	new file:   Simulated_data/slurm/slurm-7898838.out 	new file:   Simulated_data/slurm/slurm-7898844.out 	new file:   Simulated_data/slurm/slurm-7922557.out 	new file:   Simulated_data/slurm/slurm-7922558.out 	new file:   Simulated_data/slurm/slurm-7922758.out 	new file:   Simulated_data/vcf/benchmark.TIDDIT.vcf 	new file:   Simulated_data/vcf/benchmark.TIDDIT.vcf~ 	new file:   Simulated_data/vcf/benchmark.cnvnator.vcf 	new file:   Simulated_data/vcf/benchmark.dellySV.vcf 	new file:   Simulated_data/vcf/benchmark.lumpy.vcf 	new file:   Simulated_data/vcf/bnchmark_fermi.sv.vcf 	new file:   Simulated_data/vcf/candidateSV.vcf

--- Callers/DELLY.sh
+++ Callers/DELLY.sh
@@ -4,13 +4,13 @@
 #SBATCH -p core
 #SBATCH -t 4-00:00:00
 
-#bam file is argument 1, output prefix is $2
+#bam file is argument 1, reference $2 output prefix is $3
 module load bioinfo-tools
 module load delly
 module load vcftools
 
-delly -t TRA -o $2.tra.vcf -g $1
-delly -t DEL -o $2.del.vcf -g $1
-delly -t DUP -o $2.dup.vcf -g $1
-delly -t INV -o $2.inv.vcf -g $1
-vcf-concat $2.tra.vcf $2.del.vcf $2.dup.vcf $2.inv.vcf > $2.dellySV.vcf
+delly -t TRA -o $3.tra.vcf -g $2 $1
+delly -t DEL -o $3.del.vcf -g $2 $1
+delly -t DUP -o $3.dup.vcf -g $2 $1
+delly -t INV -o $3.inv.vcf -g $2 $1
+vcf-concat $3.tra.vcf $3.del.vcf $3.dup.vcf $3.inv.vcf > $3.dellySV.vcf
--- Callers/TIDDIT_simulated.sh
+++ Callers/TIDDIT_simulated.sh
@@ -7,7 +7,7 @@
 #script used to run TIDDIT, note that the TIDDIT binary variable needs to be set 
 #$1 is the bam file, second is the output prefix
 #this script is used for the simulated data, here the coverage across the simulated regions is supplied through the -c flag
-TIDDIT=/home/jesperei/FindTranslocations/bin/FindTranslocations
+TIDDIT=/home/jesperei/FindTranslocations/bin/TIDDIT
 
 module load bioinfo-tools
 module load vcftools
